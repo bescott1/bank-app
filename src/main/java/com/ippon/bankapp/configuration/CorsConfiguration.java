4bfc59b785b11807bf4bbac6fc652f8c8236353a
Ben Scott <bscott@ipponusa.com>
2021-09-21 12:09:52 -0400

    Adds cors config

---
+++ src/main/java/com/ippon/bankapp/configuration/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.ippon.bankapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
